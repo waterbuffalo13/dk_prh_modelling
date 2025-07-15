f17c38f8ffd1f6dad674ddbb840c3c0743497c0e
Patrick Fernandes <patrick.fernandes.elk@gmail.com>
2025-07-15 18:41:29 +0000

    add bookauthor table

--- models/mart/BookAuthor.sql
+++ models/mart/BookAuthor.sql
@@ -1,4 +1,10 @@
-SELECT 
+{{ config(materialized = 'table', schema = "MART") }}
+
+select distinct
     {{ dbt_utils.generate_surrogate_key([
-        'BOOK_ISBN' ]) 
-    }}, 
+        'b.BOOK_ISBN' , 'a.value'
+        ]) 
+    }} AS BOOKAUTHOR_KEY,
+    {{ dbt_utils.generate_surrogate_key(["b.BOOK_ISBN"]) }} as book_key,
+    {{ dbt_utils.generate_surrogate_key(["a.value"]) }} as author_key,
+from {{ ref("dim_book") }} b, lateral flatten(input => b.authors) a
--- tests/non_negative_transactions.sql
+++ tests/non_negative_transactions.sql
@@ -1,4 +1,4 @@
 SELECT *
 FROM {{ ref('store_sales_cleaned') }}
 WHERE 1=1 
-AND qty_sold < 0 OR price < 0 
+AND price < 0 
